8a990fd9c5ece70d6d97f90480dcdac5b36bb67c
sagibouzaglo <sagibouzaglo@campus.technion.ac.il>
2017-12-29 18:39:01 +0200

    שדג

---
+++ Colosseum 2.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Colosseum 2.0
+//
+//  Created by sagi bouzaglo on 29/12/2017.
+//  Copyright © 2017 sagi bouzaglo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
