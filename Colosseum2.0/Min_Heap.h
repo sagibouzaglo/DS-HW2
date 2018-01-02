35eaf5f003412795117dd5fb488626d6d92da9c4
sagibouzaglo <sagibouzaglo@campus.technion.ac.il>
2018-01-02 16:14:32 +0200

    Heap

---
+++ Colosseum2.0/Min_Heap.h
@@ -0,0 +1,29 @@
+//
+//  Min_Heap.h
+//  Colosseum 2.0
+//
+//  Created by sagi bouzaglo on 02/01/2018.
+//  Copyright © 2018 sagi bouzaglo. All rights reserved.
+//
+
+#ifndef Min_Heap_h
+#define Min_Heap_h
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define nullPtr 0
+
+/***************************************************************************/
+/*  Hash Table class                                                       */
+/*  types:                                                                 */
+/*  int - Key                                                              */
+/*  T - node data                                                          */
+/*  HashNode - Hash data                                                   */
+/*                                                                         */
+/*  Operations:                                                            */
+/*  insert - insert data to the table                                      */
+/*  search - check if the data already exist                               */
+/***************************************************************************/
+
+#endif /* Min_Heap_h */
