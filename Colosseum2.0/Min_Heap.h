5116453cb648edf719fe3b5c91fb875b1a188d29
sagibouzaglo <sagibouzaglo@campus.technion.ac.il>
2018-01-05 13:08:06 +0200

    Min Heap

--- Colosseum2.0/Min_Heap.h
+++ Colosseum2.0/Min_Heap.h
@@ -5,17 +5,15 @@
 //  Created by sagi bouzaglo on 02/01/2018.
 //  Copyright © 2018 sagi bouzaglo. All rights reserved.
 //
-
 #ifndef Min_Heap_h
 #define Min_Heap_h
 
 #include <stdlib.h>
 #include <stdio.h>
-
 #define nullPtr 0
 
 /***************************************************************************/
-/*  Hash Table class                                                       */
+/*  Min Heap class                                                         */
 /*  types:                                                                 */
 /*  int - Key                                                              */
 /*  T - node data                                                          */
@@ -26,4 +24,57 @@
 /*  search - check if the data already exist                               */
 /***************************************************************************/
 
+template <class T>
+class MinHeap {
+    int tableSize;
+    int* lastIn;
+    T* heap;
+    
+    void siftDown(){
+    
+    }
+    public:
+    // construct zero initialized Min Heap of size
+    HeapTable(int const size) {
+        tableSize=size;
+        table = new HashNode<T> *[size]();
+        for (int i = 0; i < size; i++){
+            table[i] = nullptr;
+        }
+        lastIn=heap;
+    }
+
+    /* Description:   This function inserts new data to the Heap
+     * Input:         Data
+     *                Compare function
+     * Output:        None.
+     * Exceptions:
+     * Return Values: true if insert succeded, false if not
+     */
+    template <class Compare>
+    bool insert(const T &data, const Compare& compare){
+        
+    }
+    
+    /* Description:   This function find the min of the Heap
+     * Input:
+     * Output:        None.
+     * Exceptions:
+     * Return Values: pointer to the minimum of the heap, null if empty
+     */
+    T* findMin(){
+        
+    }
+    
+    /* Description:   This function find the min of the Heap and delete it
+     * Input:
+     * Output:        None.
+     * Exceptions:
+     * Return Values: None.
+     */
+    template <class Compare>
+    void delMin(const Compare& compare){
+        
+    }
+};
 #endif /* Min_Heap_h */
