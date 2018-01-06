68ac0071c77f848f4084af70d7ffab2d3191abeb
sagibouzaglo <sagibouzaglo@campus.technion.ac.il>
2018-01-06 15:34:12 +0200

    finally

---
+++ Colosseum2.0/Colosseum2.cpp
@@ -0,0 +1,9 @@
+//
+//  Colosseum2.cpp
+//  Colosseum 2.0
+//
+//  Created by sagi bouzaglo on 06/01/2018.
+//  Copyright © 2018 sagi bouzaglo. All rights reserved.
+//
+
+#include "Colosseum2.hpp"
---
+++ Colosseum2.0/Colosseum2.h
@@ -0,0 +1,13 @@
+//
+//  Colosseum2.h
+//  Colosseum 2.0
+//
+//  Created by sagi bouzaglo on 06/01/2018.
+//  Copyright © 2018 sagi bouzaglo. All rights reserved.
+//
+
+#ifndef Colosseum2_h
+#define Colosseum2_h
+
+
+#endif /* Colosseum2_h */
